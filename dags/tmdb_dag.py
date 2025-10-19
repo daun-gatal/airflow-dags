11f463c734212fa6b0057bb5a54260a6e739a821
daun-gatal <rsudo54@gmail.com>
2025-10-19 17:37:34 +0700

    add kafka dag

--- dags/tmdb_dag.py
+++ dags/tmdb_dag.py
@@ -8,6 +8,7 @@ import requests
 def get_movie_ids(start_date: str, end_date: str, api_key: str, base_url: str):
     import random
     import time
+    import json
 
     page = 1
 
@@ -37,7 +38,8 @@ def get_movie_ids(start_date: str, end_date: str, api_key: str, base_url: str):
         data = response.json()
 
         for movie in data.get("results", []):
-            yield _get_movie_details(movie["id"], details_url, headers)
+            details = _get_movie_details(movie["id"], details_url, headers)
+            yield (movie["id"], json.dumps(details))
             time.sleep(random.randint(3, 5))  # To respect rate limits
 
         if page >= data.get("total_pages", 0):
@@ -61,12 +63,12 @@ with DAG(
         kafka_config_id="kafka_conn",
         topic="tmdb",
         producer_function=get_movie_ids,
-        producer_function_args=[
-            "{{ (macros.datetime.strptime(ds, '%Y-%m-%d') + macros.timedelta(days=-1)).strftime('%Y-%m-%d') }}",
-            "{{ ds }}",
-            "{{ var.value.TMDB_API_KEY }}",
-            "{{ var.value.TMDB_API_BASE_URL}}",
-        ],
+        producer_function_kwargs={
+            "api_key": "{{ var.value.TMDB_API_KEY }}",
+            "base_url": "{{ var.value.TMDB_API_BASE_URL}}",
+            "start_date": "{{ (macros.datetime.strptime(ds, '%Y-%m-%d') + macros.timedelta(days=-1)).strftime('%Y-%m-%d') }}",
+            "end_date": "{{ ds }}",
+        },
     )
 
     start >> produce_to_kafka >> end
